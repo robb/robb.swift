53e34060adc142e5c65e58ec83f605406db19dee
Robert Böhnke <robb@robb.is>
2019-06-13 14:47:36 +0200

    Add filters to modify document as needed

---
+++ Sources/robb.swift/InlineFilter.swift
@@ -0,0 +1,20 @@
+import Foundation
+import HTML
+
+struct InlineFilter: Filter {
+    var baseURL: URL
+
+    func apply(node: Node) -> Node? {
+        if let node = node as? Tag, node.name == "custom-inline", let path = node.attributes["path"] {
+            let url = URL(fileURLWithPath: path, relativeTo: baseURL)
+
+            return try! Text(value: String(contentsOf: url))
+        }
+
+        return node
+    }
+
+    static func inline(file path: String) -> Node {
+        return Tag(name: "custom-inline", attributes: [ "path": path ], children: [])
+    }
+}

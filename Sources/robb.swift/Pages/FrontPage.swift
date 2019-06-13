320997a7efb0b064536635019dcede2a62358a06
Robert Böhnke <robb@robb.is>
2019-06-13 14:52:23 +0200

    Use InlineFilter.inline to pull in css and js files

--- Sources/robb.swift/Pages/FrontPage.swift
+++ Sources/robb.swift/Pages/FrontPage.swift
@@ -10,69 +10,7 @@ struct FrontPage: Page {
         defaultLayout(page: self) {
             header(id: "header") {
                 style(type: "text/css") {
-                    ###"""
-                @keyframes fadeIn {
-                    from { opacity: 0; }
-                    to { opacity: 1; }
-                }
-
-                @keyframes shake {
-                      0% { transform: rotate(  0.0deg); }
-                     20% { transform: rotate(  7.0deg); }
-                     40% { transform: rotate(-10.0deg); }
-                     60% { transform: rotate( 11.0deg); }
-                     80% { transform: rotate( -9.0deg); }
-                    100% { transform: rotate(  0.0deg); }
-                }
-
-                #intro {
-                    margin: 7.5rem 0 1.25rem 0;
-                }
-
-                @media (max-height: 680px) {
-                    #intro {
-                        margin-top: 5rem;
-                    }
-                }
-
-                #intro .summary {
-                    border-bottom: 1px dashed;
-                    cursor: pointer;
-                }
-
-                #intro p {
-                    display: inline;
-                    font-size: 1.8rem;
-                    font-weight: 400;
-                    hyphens: manual;
-                    line-height: 3rem;
-                }
-
-                #intro p:after {
-                    content: " ";
-                }
-
-                #intro a {
-                    border-bottom: 1px solid rgba(0, 0, 0, 1);
-                    cursor: pointer;
-                    text-decoration: none;
-                }
-
-                #intro .shake {
-                    animation-delay: 2.1s;
-                    animation-duration: 1s;
-                    animation-name: shake;
-                    animation-timing-function: ease-out;
-                    display: inline-block;
-                    transform-origin: 70% 70%;
-                }
-
-                #intro .strikethrough {
-                    text-decoration: line-through;
-                    text-decoration-color: rgb(219, 61, 61);
-                    -webkit-text-decoration-color: rgb(219, 61, 61);
-                }
-                """###
+                    InlineFilter.inline(file: "css/intro.css")
                 }
 
                 section(id: "intro") {
@@ -121,32 +59,7 @@ struct FrontPage: Page {
                 }
 
                 script(type: "text/javascript") {
-                    ###"""
-                    "use strict";
-
-                        var intro = document.querySelector("#intro");
-
-                        intro
-                            .querySelectorAll(".token")
-                            .forEach(function(element) {
-                                var node = document.createElement("span");
-
-                                node.appendChild(document.createTextNode(element.getAttribute("data-summary")));
-                                node.classList.add("summary");
-
-                                var parent = element.parentElement;
-
-                                node.onclick = function() {
-                                    intro.classList.add("activated");
-
-                                    node.replaceWith(element);
-
-                                    element.style.animationDuration = "0.4s";
-                                    element.style.animationName = "fadeIn";
-                                };
-                                element.replaceWith(node);
-                            });
-                    """###
+                    InlineFilter.inline(file: "js/intro.js")
                 }
             }
         }

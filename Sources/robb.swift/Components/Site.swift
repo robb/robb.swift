6fb2ad3fdb4062540c9e4616e62a99aad82955af
Robert Böhnke <robb@robb.is>
2020-03-11 20:32:02 +0100

    Inline Site properties

--- Sources/robb.swift/Components/Site.swift
+++ Sources/robb.swift/Components/Site.swift
@@ -3,36 +3,19 @@ import Foundation
 struct Site: ResourceGenerator {
     let baseURL: URL
 
-    let filters: [Filter]
-
-    let outputDirectory: URL
-
-    let pageGenerators: [PageGenerator]
-
-    let resourceGenerators: [ResourceGenerator]
+    var outputDirectory: URL {
+        baseURL.appendingPathComponent("Site")
+    }
 
     init(baseDirectory: URL) throws {
         baseURL = baseDirectory
-
-        filters = [
-            InlineFilter(baseURL: baseURL.appendingPathComponent("Inline")),
-            MarkdownFilter(),
-            PrismFilter(),
-            DependencyFilter()
-        ]
-
-        pageGenerators = [
-            JekyllPostGenerator(directory: baseURL.appendingPathComponent("Posts"))
-        ]
-
-        resourceGenerators = [
-            StaticFileGenerator(directory: baseURL.appendingPathComponent("Resources"))
-        ]
-
-        outputDirectory = baseURL.appendingPathComponent("Site")
     }
 
     func generate() throws -> [Resource] {
+        let pageGenerators = [
+            JekyllPostGenerator(directory: baseURL.appendingPathComponent("Posts"))
+        ]
+
         let pages = try pageGenerators.flatMap { try $0.generate() }
 
         let posts = pages.compactMap { $0 as? Post }
@@ -51,9 +34,18 @@ struct Site: ResourceGenerator {
             TakingPictures(posts: posts.filter { $0.category == "taking-pictures" })
         ]
 
-        let filters = self.filters
+        let filters: [Filter] = [
+            InlineFilter(baseURL: baseURL.appendingPathComponent("Inline")),
+            MarkdownFilter(),
+            PrismFilter(),
+            DependencyFilter()
+        ]
 
         let pageResources = allPages.concurrentMap { $0.applyFilters(filters) }
+
+        let resourceGenerators = [
+            StaticFileGenerator(directory: baseURL.appendingPathComponent("Resources"))
+        ]
 
         let fileResources = try resourceGenerators.flatMap { try $0.generate() }
 

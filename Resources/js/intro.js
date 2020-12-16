9af5cf3ac0e032adbd77ff1bf4a8bf624e065210
Robert Böhnke <robb@robb.is>
2020-12-16 16:54:13 +0100

    Make frontpage more accessible

--- Resources/js/intro.js
+++ Resources/js/intro.js
@@ -7,13 +7,14 @@ intro
     .forEach(function(element) {
         let node = document.createElement("span");
         node.setAttribute("role", "button");
+        node.setAttribute("tabindex", "0");
 
         node.appendChild(document.createTextNode(element.getAttribute("data-summary")));
         node.classList.add("summary");
 
         let parent = element.parentElement;
 
-        node.onclick = function() {
+        let handler = function() {
             intro.classList.add("activated");
 
             node.replaceWith(element);
@@ -21,5 +22,13 @@ intro
             element.style.animationDuration = "0.4s";
             element.style.animationName = "fadeIn";
         };
+
+        node.addEventListener("click", handler);
+        node.addEventListener("keydown", function(event) {
+            if (event.keyCode === 32 || event.keyCode === 13) {
+                handler();
+            }
+        });
+
         element.replaceWith(node);
     });

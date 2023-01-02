c04f5d6686673adbb70079c3dda514920f0ef8ca
David v.Knobelsdorff <dehlen@users.noreply.github.com>
2023-01-02 20:31:41 +0100

    fix deprecation warning

--- Sources/robb.swift/Extensions/URL+MIMEType.swift
+++ Sources/robb.swift/Extensions/URL+MIMEType.swift
@@ -1,12 +1,8 @@
 import Foundation
+import UniformTypeIdentifiers
 
 extension URL {
     var mimeType: String? {
-        if let uti = UTTypeCreatePreferredIdentifierForTag(kUTTagClassFilenameExtension, pathExtension as CFString, nil)?.takeUnretainedValue(),
-           let mime = UTTypeCopyPreferredTagWithClass(uti, kUTTagClassMIMEType) {
-            return String(mime.takeUnretainedValue())
-        }
-
-        return nil
+        UTType(filenameExtension: pathExtension)?.preferredMIMEType
     }
 }
